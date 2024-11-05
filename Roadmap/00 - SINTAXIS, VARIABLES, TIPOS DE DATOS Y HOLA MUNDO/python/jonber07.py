4cc04a48f3235a98a988047297d408fa65772f32
jonber07 <106829868+jonber07@users.noreply.github.com>
2024-11-05 00:14:57 -0500

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jonber07.py
@@ -0,0 +1,32 @@
+#PÁGINA WEB OFICIAL: https://python.org
+
+#Comentario en una línea
+
+"""
+Esto también es una forma de hacer comentario
+en una o más líneas
+"""
+
+'''
+Las comillas simples también es 
+otra forma de hacer comentarios de varias líneas
+'''
+
+mi_variable = "Mi Variable"
+
+mi_variable = "Nuevo valor de mi variable"
+ 
+MI_CONSTANTE = "Mi Constante" # por convención
+MI_CONSTANTE = "ASDFGHJHJ"
+
+# DATOS PRIMITIVOS
+
+entero:int = 32
+flotante:float= 345.5
+cadena:str = "Cadena"
+booleano:bool = False
+booleano2:bool = True
+
+print('hola, Python!')
+print(type(booleano),booleano)
+print(type(entero),entero)
